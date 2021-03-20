e5f20bfff212db1c77c42759ba63a23c47671d51
Moshe Schmidt <Smith00101010@users.noreply.github.com>
2021-03-20 12:19:36 +0100

    Add Tests for the rate limiting strategies

---
+++ src/TvMaze.Api.Client.Tests/RateLimitingStrategyTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Flurl.Http.Testing;
+using TvMaze.Api.Client.Configuration;
+using TvMaze.Api.Client.Exceptions;
+using Xunit;
+
+namespace TvMaze.Api.Client.Tests
+{
+    public class RateLimitingStrategyTests : IDisposable
+    {
+        private readonly HttpTest _httpTest;
+
+        public RateLimitingStrategyTests()
+        {
+            _httpTest = new HttpTest();
+            _httpTest.RespondWith(string.Empty, RateLimitingConstants.StatusCode);
+        }
+        
+        [Fact]
+        public async void ThrowExceptionRateLimitingStrategy()
+        {
+            // arrange
+            var strategy = new ThrowExceptionRateLimitingStrategy();
+            var tvMazeClient = new TvMazeClient(new HttpClient(), strategy);
+            var beforeCount = _httpTest.CallLog.Count;
+
+            // act
+            Func<Task> action = async () =>
+            {
+                await tvMazeClient.Shows.GetShowMainInformationAsync(1);
+            };
+
+            // assert
+            await action.Should().ThrowAsync<UnexpectedResponseStatusException>();
+            _httpTest.CallLog.Count.Should().Be(beforeCount + 1);
+        }
+
+        [Fact]
+        public async void RetryRateLimitingStrategy()
+        {
+            // arrange
+            const int expectedRetries = 5;
+            var strategy = new RetryRateLimitingStrategy(expectedRetries, TimeSpan.FromMilliseconds(1));
+            var tvMazeClient = new TvMazeClient(new HttpClient(), strategy);
+            var beforeCount = _httpTest.CallLog.Count;
+
+            // act
+            Func<Task> action = async () =>
+            {
+                await tvMazeClient.Shows.GetShowMainInformationAsync(1);
+            };
+
+            // assert
+            await action.Should().ThrowAsync<UnexpectedResponseStatusException>();
+            _httpTest.CallLog.Count.Should().Be(beforeCount + expectedRetries + 1);
+        }
+
+        public void Dispose()
+        {
+            _httpTest?.Dispose();
+        }
+    }
+}
